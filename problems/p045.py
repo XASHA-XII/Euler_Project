9bc5d1049f86f0e1c03ebc387b066593f594206b
Rob <barbarino.robin@gmail.com>
2023-02-07 23:27:32 +0100

    Modifications to p045.

--- problems/p045.py
+++ problems/p045.py
@@ -1,28 +1,20 @@
 import itertools
 from math import isqrt
 
+'''
+https://projecteuler.net/problem=45
+Triangular, pentagonal, and hexagonal
+Triangular:     T(n)=n(n+1)/2
+Pentagonal:     P(n)=n(3n-1)/2
+HexagonaL:      H(n)=n(2n-1)
+'''
+
+
 
 def hexagonal(n: int) -> int:
     '''Returns the n-th hexagonal number.'''
     H_n = n * (2*n - 1)
     return H_n
-
-
-def is_triangular(x: int) -> bool:
-    '''
-    Returns whether x is a triangle number, i.e.  
-            x = (n * (n + 1)) // 2.
-    This is verified by taking the inverse function
-            n= 1/2 (-1 + sqrt(1 + 8 x))
-    and checking whether all terms involved are integer.
-    '''
-    interim_result = isqrt(1 + 8*x)
-    if (interim_result**2 == 1 + 8 * x):
-        result = ((-1 + interim_result) % 2 == 0)
-    else:
-        result = False
-    return result
-
 
 def is_pentagonal(x: int) -> int:
     '''
@@ -43,12 +35,14 @@ def is_pentagonal(x: int) -> int:
 def main() -> int:
     '''Returns the second number that is triangular, pentagonal and hexagonal by looping through
      all hexagonal numbers and checking if they're triangular and pentagonal. The first such number is
-     H(143)=40755 
+     H(143)=40755.
+     Since T(2m-1) = 2m*(2m-1) = H(n) 
+     all hexagonal numbers are triangular numbers and we do not need to check them.
      '''
 
     N = 144
     for H_n in (hexagonal(k) for k in itertools.count(N)):
-        if is_triangular(H_n) and is_pentagonal(H_n):
+        if is_pentagonal(H_n):
             result = H_n
             return result
 
